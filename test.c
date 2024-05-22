b3e3b48d72027ae37a8276ff31e1d4c342c51813
Nguyen Dac Quan <negeriov@gmail.com>
2024-05-22 22:53:25 +0700

    a

--- test.c
+++ test.c
@@ -1,7 +1,18 @@
 #include <stdlib.h>
+#include <stdio.h>
+
+void bar(void *ptr) {
+    free(ptr);
+}
+
+void foo(void *ptr) {
+    int a;
+    scanf("%d", &a);
+    if(a) bar(ptr);
+    else exit(1);
+}
 
 int main() {
     void *ptr = malloc(10000);
-    free(ptr);
     free(ptr);
 }
